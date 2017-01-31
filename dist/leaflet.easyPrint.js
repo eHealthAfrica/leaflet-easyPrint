91a0707de1104a562eb468a18e18798999c88eb4
Musa Musa <musakunte@gmail.com>
2017-01-31 09:05:35 +0100

    added conditions to check if option is set

--- dist/leaflet.easyPrint.js
+++ dist/leaflet.easyPrint.js
@@ -1,54 +1,60 @@
 L.Control.EasyPrint = L.Control.extend({
-	options: {
-		title: 'Print map',
-		position: 'topleft'
-	},
-
-	onAdd: function () {
-    var container = getContainter(this.options);
-		this.link = getPrintLink(this.options, container);
-
-		L.DomEvent.addListener(this.link, 'click', printPage, this.options);
-
-		return container;
-	}
-
+  options: {
+    title: 'Print map',
+    position: 'topleft'
+  },
+  
+  onAdd: function () {
+    var container = getContainter(this.options, L);
+    this.link = getPrintLink(this.options, container);
+    
+    L.DomEvent.addListener(this.link, 'click', printPage, this.options);
+    
+    return container;
+  }
+  
 });
 
 L.easyPrint = function(options) {
-	return new L.Control.EasyPrint(options);
+  return new L.Control.EasyPrint(options);
 };
 
 function printPage () {
-
-	if (this.elementsToHide) {
-		var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);  
-
-		for (var i = 0; i < htmlElementsToHide.length; i++) {
-			htmlElementsToHide[i].className = htmlElementsToHide[i].className + ' _epHidden';
-		}
-	}
-	window.print();
-
-	if (this.elementsToHide){
-		var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);  
-
-		for (var i = 0; i < htmlElementsToHide.length; i++) {
-			htmlElementsToHide[i].className = htmlElementsToHide[i].className.replace(' _epHidden','');
-		}
-	}
+  
+  if (this.elementsToHide) {
+    var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);
+    
+    for (var i = 0; i < htmlElementsToHide.length; i++) {
+      htmlElementsToHide[i].className = htmlElementsToHide[i].className + ' _epHidden';
+    }
+  }
+  window.print();
+  
+  if (this.elementsToHide){
+    var htmlElementsToHide = document.querySelectorAll(this.elementsToHide);
+    
+    for (var i = 0; i < htmlElementsToHide.length; i++) {
+      htmlElementsToHide[i].className = htmlElementsToHide[i].className.replace(' _epHidden','');
+    }
+  }
 }
 
-function getContainter (options) {
-  var element = options.controlElement | 'div';
-  var elementClasses = options.controlElementClasses | 'leaflet-control-easyPrint leaflet-bar leaflet-control';
-  return L.DomUtil.create(element, elementClasses);
+function getContainter (options, L) {
+  var container;
+  var element = options.controlElement ? options.controlElement : 'div';
+  var elementClasses = options.controlElementClasses ? options.controlElementClasses : 'leaflet-control-easyPrint leaflet-bar leaflet-control';
+  try {
+    container = L.DomUtil.create(element, elementClasses);
+  } catch(e) {
+    container = L.DomUtil.create('div', elementClasses);
+  }
+  return container
 }
 
 function getPrintLink (options, container) {
-  var linkClasses = options.linkClasses | 'leaflet-control-easyPrint-button leaflet-bar-part';
+  var linkClasses = options.linkClasses ? options.linkClasses : 'leaflet-control-easyPrint-button leaflet-bar-part';
   var link = L.DomUtil.create('a', linkClasses, container);
-  link.id = options.linkId | "leafletEasyPrint";
+  link.id = options.linkId ? options.linkId : 'leafletEasyPrint';
   link.title = options.title;
   return link
 }
